39a38984647923c69c29acc4ecc57c8688752522
Nguyễn Văn Thiều <117129574+NguyenThieu2803@users.noreply.github.com>
2024-10-19 20:22:04 +0700

    create RestAPI for Cart

--- furnitureapp/lib/config/config.dart
+++ furnitureapp/lib/config/config.dart
@@ -3,4 +3,5 @@ class Config {
   static const String apiURL = "10.0.2.2:3000"; // Sử dụng 10.0.2.2 cho localhost trên giả lập Android
   static const String registerAPI = "api/auth/register";
   static const String loginAPI = "api/auth/login";
+  static const String getCartAPI = "api/cart";
 }
